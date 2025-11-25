10e35f9b76972a07b47ae380ed2c710bdf6b5719
obact <sss5642195@gmail.com>
2025-11-25 04:01:14 +0000

    Updates

---
+++ 1bfc9850-f74d44f84118eba6cdd0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkobact=self.webpackChunkobact||[]).push([[379],{1612:function(c,t,a){a.d(t,{uCC:function(){return u}});var n=a(4084);function u(c){return(0,n.k5)({tag:"svg",attr:{viewBox:"0 0 448 512"},child:[{tag:"path",attr:{d:"M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"}}]})(c)}}}]);
+//# sourceMappingURL=1bfc9850-f74d44f84118eba6cdd0.js.map
